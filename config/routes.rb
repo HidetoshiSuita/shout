a387064bf2cbbc7c395db7f6049cd2b21a47f9f4
KantaSunaga <kanta@sunagakanta-no-MacBook-Air.local>
2016-12-10 10:33:29 +0900

    devise指定設定ができていなかったので、再びコミット

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
 
   get 'pages/show'
 
+  root 'pages#index'
+  get 'pages/show'
+　#↑deviseの指定設定
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
