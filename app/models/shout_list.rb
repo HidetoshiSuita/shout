9a823ce45d7b036f13648e805f4f99026003463b
KantaSunaga <kanta@sunagakanta-no-MacBook-Air.local>
2016-12-15 09:54:21 +0900

    shout_list_modelにcreate_shout_listメソッドを追加

--- app/models/shout_list.rb
+++ app/models/shout_list.rb
@@ -1,2 +1,8 @@
 class ShoutList < ApplicationRecord
+  def create_shout_list(contents, usr_id)
+    ShoutList.create(
+                     :shout => contents,
+                     :user_id =>usr_id
+                     )
+  end
 end
