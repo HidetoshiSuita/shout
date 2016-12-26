64579227c68ae533306c300cbea854fa5326586e
KantaSunaga <kanta@sunagakanta-no-MacBook-Air.local>
2016-12-26 10:25:34 +0900

    ユーザー検索機能追加（検索機能はリンクにはなっていない）

--- app/controllers/after_log_in_prosess_controller.rb
+++ app/controllers/after_log_in_prosess_controller.rb
@@ -1,22 +1,23 @@
 class AfterLogInProsessController < ApplicationController
-   before_action :login_check, only: :shout
+  before_action :login_check, only: :shout
 
-  def chouse
-  end
+ def choused
+ end
 
-  def shout
-    @user = ShoutList.new
-  end
+ def shout
+   @user = ShoutList.new
+ end
 
-  def shout_aftre
-  end
+ def shout_aftre
+ end
 
-  def watch_shout
-      my_shout = ShoutList.where(:user_id => current_user.id)
-      @shout = my_shout.sort_by{|shout_info| shout_info[:cretate_at]}.reverse!
-  end
+ def watch_shout
+     my_shout = ShoutList.where(:user_id => current_user.id)
+     @shout = my_shout.sort_by{|shout_info| shout_info[:cretate_at]}.reverse!
+ end
 
   def find_user
-
+    @q = User.search(params[:q])
+    @users = @q.result(distinct: true)
   end
 end
