3e7d17cc9d3850675247763f56348267f81b878b
t-tsukamoto <t-tsukamoto@ankh-systems.co.jp>
2017-03-09 11:19:43 +0900

    メール設定の書き換え

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,7 +25,6 @@ Rails.application.configure do
     }
   else
     config.action_controller.perform_caching = false
-
     config.cache_store = :null_store
   end
 
@@ -44,17 +43,19 @@ Rails.application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
+  
   config.assets.debug = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
-	address:               'smtp.gmail.com',
-	port:      	           587,
-	domain:                'gmail.com',
-	user_name:             'shoutunnei@gmail.com',
-	password:              '',
-	authentication:        'plain',
-	enable_starttls_auto:  true  }
+  config.action_mailer.smtp_settings = {
+  	address:               'smtp.gmail.com',
+  	port:      	           587,
+  	domain:                'gmail.com',
+  	user_name:             ENV['MAIL_USER_NAME'],
+  	password:              ENV['MAIL_PASSWORD'],
+  	authentication:        'plain',
+  	enable_starttls_auto:  true  
+  }
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
