2a8c6d8652fc793c65f103a2f20d0b689937a729
t-tsukamoto <t-tsukamoto@ankh-systems.co.jp>
2017-03-09 12:49:48 +0900

    modify

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,8 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
+  config.serve_static_assets = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -57,7 +58,6 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "shout_#{Rails.env}"
   config.action_mailer.perform_caching = false
   
-  config.assets.debug = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
