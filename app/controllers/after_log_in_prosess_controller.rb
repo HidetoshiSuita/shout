b7264c85050080ad53d22440c0c25233942ebbbd
t-tsukamoto <t-tsukamoto@ankh-systems.co.jp>
2017-03-26 22:12:01 +0900

    watch shout

--- app/controllers/after_log_in_prosess_controller.rb
+++ app/controllers/after_log_in_prosess_controller.rb
@@ -63,7 +63,7 @@ class AfterLogInProsessController < ApplicationController
 
  def watch_shout
    follow_list = FollowList.get_follow_info_list(current_user.id)
-   @shout_list = ShoutList.where(:user_id => follow_list).where(resp_shout: nil).order(created_at: :desc)
+   @shout = ShoutList.where(:user_id => follow_list).where(resp_shout: nil).order(created_at: :desc)
    @resp_shout = ShoutList.where(:user_id => follow_list).where.not(resp_shout: nil)
  end
  
